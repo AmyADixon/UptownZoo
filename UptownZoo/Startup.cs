9bc283b3417f3f95d280b0861f9e3c20a7b59e5e
AmyADixon <amysha.dixon@gmail.com>
2018-09-24 14:18:01 -0700

    Add project files.

---
+++ UptownZoo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UptownZoo.Startup))]
+namespace UptownZoo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
