ad4e89b7383396ddb69df7f83a5d5d96e4a2d5f5
AmyADixon <amysha.dixon@gmail.com>
2018-10-10 14:41:52 -0700

    Published website and connected to azure sql db

---
+++ UptownZoo/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace UptownZoo.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<UptownZoo.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(UptownZoo.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
